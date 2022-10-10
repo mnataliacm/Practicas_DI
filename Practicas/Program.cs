68174333524aaac5d47e5ffae2f17f435a0a9072
Natalia CM <mnacamu@gmail.com>
2022-10-10 13:34:42 +0200

    Practica 1

--- Practicas/Program.cs
+++ Practicas/Program.cs
@@ -10,6 +10,26 @@ namespace Practicas
     {
         static void Main(string[] args)
         {
+            Practica1();
         }
+
+
+
+        #region Practica 2
+
+
+
+        #endregion
+
+        #region Practica 1
+        static void Practica1()
+        {
+            Console.WriteLine("Inroduce un radio: ");
+            int num1 = int.Parse(Console.ReadLine());
+            const double PI = 3.14;
+            double area = PI * Math.Pow(num1, 2);
+            Console.WriteLine($"El área del círculo con un radio de: {num1} vale {area}");
+        }
+        #endregion
     }
 }
